926b09fc74c5e291351d381e0be0c1a4fba3992e
Saksham Tripathi <krishna.tripathi0710@gmail.com>
2026-02-14 01:46:54 -0500

    Update f1rfmodel.py

--- f1rfmodel.py
+++ f1rfmodel.py
@@ -1,4 +1,4 @@
-#imports (make sure the library is installed on your machine)
+#imports (make sure the library is installed on your machine or in a required list)
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
@@ -22,9 +22,9 @@ df["SessionType"] = df["Session"].map({"Q":0, "R":1}).astype(float)
 features = ["Driver", "AirTemp", "TrackTemp", "EventID", "SessionType"]
 target = "LapTime"
 
-df = df[features + [target]].dropna()
+df = df[features + [target]].dropna() #remove empty data
 
-split_index = int(0.8 * len(df))
+split_index = int(0.8 * len(df)) #split data to 80:20 train:test split
 
 
 #train-test split
@@ -37,6 +37,7 @@ y_train = train_df[target]
 X_test = test_df[features]
 y_test = test_df[target]
 
+#defining model object as rf regressor
 model = RandomForestRegressor(
     n_estimators=100,
     max_depth=8,
@@ -46,7 +47,7 @@ model = RandomForestRegressor(
 model.fit(X_train, y_train)
 
 predictions = model.predict(X_test)
-mae = mean_absolute_error(y_test, predictions)
+mae = mean_absolute_error(y_test, predictions) #mae to model accuracy (lower mae = higher accuracy)
 
 print(f"training complete | mean absolute error = {mae}.")
 
@@ -57,8 +58,10 @@ print("known event (copy and paste special characters):")
 for e in event_to_id:
     print("-", e)
 
+#mapping session type (quali vs racing) to numerical values for training
 sesh_type_mapping = {"Q":0, "R":1}
 
+#try case for user data input
 try:
     driver = int(input("driver number (name respective to season): "))
     air_temp = float(input("air temperature: "))
@@ -66,10 +69,12 @@ try:
     event_name = input("event name (case sensitive): ")
     sesh_type = input("session type (Q for qualifying and R for race | case sensitive): ")
 
+#incorrect input feedback
     if event_name not in event_to_id:
         print("event not in training data")
     elif sesh_type not in sesh_type_mapping:
         print("invalid session type")
+#rf prediction call
     else:
         sesh_num = sesh_type_mapping[sesh_type]
         event_id = event_to_id[event_name]
